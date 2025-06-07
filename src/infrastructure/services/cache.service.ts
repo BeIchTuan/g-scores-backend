f38394bd0bedafe373860ca85d76378d79512afe
BeIchTuan <22521595@gm.uit.edu.vn>
2025-06-07 07:53:37 +0700

    refactor: change redis url for deploying

--- src/infrastructure/services/cache.service.ts
+++ src/infrastructure/services/cache.service.ts
@@ -6,13 +6,11 @@ class CacheService {
   private static instance: CacheService;
 
   private constructor() {
-    this.redis = new Redis({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: Number(process.env.REDIS_PORT) || 6379,
+    this.redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379', {
       retryStrategy: (times) => {
         const delay = Math.min(times * 50, 2000);
         return delay;
-      }
+      },
     });
 
     this.redis.on('error', (err) => {
